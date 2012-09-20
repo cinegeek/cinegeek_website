c1f54516d83b165c9e7c7c2039f17ec1babe36b5
辻本 侑平 <yuhhhht@gmail.com>
2012-09-21 03:56:21 +0900

    rotation

--- js/main.js
+++ js/main.js
@@ -57,18 +57,64 @@ function threeStart() {
     initScene();
     initLight();
     initObject();
-
     renderer.clear();
     renderer.render(scene, camera);
     loop();
 }
-var cube_rotation_arr = [0,0,0];
-var cube_dir_arr = [0,0,600];
+var now_cube_rotation_arr = [new Array(0,0,0),new Array(0,0,0),new Array(0,0,0)];
+var now_camera_dir_arr = [0,0,600];
+var to_cube_rotation_arr = [new Array(0,0,0),new Array(0,0,0),new Array(0,0,0)];
+var to_camera_dir_arr = [0,0,600];
+
 function loop() {
     if(!move_flg) return;
-    cube_arr[0].rotation.set(0 ,0, 0);
-    camera.position.set(0,0,600);
+    for(var i = 0 ; i< 3 ; i++){
+        for(var j = 0 ; j<3 ; j++){
+            now_cube_rotation_arr[i][j] += (to_cube_rotation_arr[i][j] - now_cube_rotation_arr[i][j])*0.2;
+            now_camera_dir_arr[i][j] += (to_camera_dir_arr[i][j] - now_camera_dir_arr[i][j])*0.2;
+        }
+        cube_arr[i].rotation.set(now_cube_rotation_arr[i][0] ,now_cube_rotation_arr[i][1],now_cube_rotation_arr[i][2]);
+    }
+    camera.position.set(now_camera_dir_arr[0],now_camera_dir_arr[1],now_camera_dir_arr[2]);
+    camera.lookAt({x:0,y:0,z:0});
 	renderer.clear();
 	renderer.render(scene, camera);
 	window.requestAnimationFrame(loop);
 }
+var speed_arr = [10,90,50];
+function switchCube(num){
+
+    for(var i = 0 ;i< 3 ; i++){
+        var per = speed_arr[Math.floor(Math.random()*3)];
+        console.log("-----" + Math.floor(Math.random()*3));
+        switch(num){
+            case 1:
+                to_cube_rotation_arr[i] = [per*Math.PI/4*1,per*Math.PI/4*3,per*Math.PI/4*1];
+                to_camera_dir_arr = [0,0,600];
+            break;
+            case 2:
+                to_cube_rotation_arr[i] = [per*50*Math.PI/4,per*50*Math.PI/4,per*50*Math.PI/4*3];
+                to_camera_dir_arr = [0,0,600];
+            break;
+            case 3:
+                to_cube_rotation_arr[i] = [50*Math.PI/4*3,50*Math.PI/4*2,50*Math.PI/4*2];
+                to_camera_dir_arr = [0,0,600];
+            break;
+            case 4:
+                to_cube_rotation_arr[i] = [50*Math.PI/4*1,50*Math.PI/4*2,50*Math.PI/4*2];
+                to_camera_dir_arr = [0,0,600];
+            break;
+            case 5:
+                to_cube_rotation_arr[i] = [50*Math.PI,50*Math.PI,50*Math.PI];
+                to_camera_dir_arr = [0,0,600];
+            break;
+            case 6:
+                to_cube_rotation_arr[i] = [50*Math.PI/4*2,50*Math.PI/4*4,50*Math.PI/4*2];
+                to_camera_dir_arr = [0,0,600];
+            break;
+        }    
+    }
+}
+
+
+
